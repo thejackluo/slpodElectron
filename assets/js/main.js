50758d9bf30d63b1fccfbc53bd64c32378353792
HJCJackLuo <30420522+HJCJackLuo@users.noreply.github.com>
2020-05-16 14:27:37 -0700

    More changes I guess

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,7 @@
 // Modules to control application life and create native browser window
-const electron = require('electron');
+const {app, BrowserWindow, Menu} = require('electron');
 const url = require('url');
 const path = require('path');
-
-const {app, BrowserWindow, Menu} = electron;
-
 
 function createWindow () {
   // Create the browser window.
@@ -14,10 +11,10 @@ function createWindow () {
     webPreferences: {
       preload: path.join(__dirname, 'assets/js/preload.js')
     }
-  })
+  });
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('index.html');
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -33,21 +30,23 @@ app.whenReady().then(() => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+  });
+});
 
 
 app.on('ready', function() {
+  
+  
   const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
   Menu.setApplicationMenu(mainMenu);
-})
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') app.quit()
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
